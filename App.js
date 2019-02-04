528b8b0f6f8d02269ed3cda49a6d78634eec0631
Renan San <renan.santos@sabion.com.br>
2019-02-04 14:36:57 -0200

    change some static by dynamic data using navigator

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, FlatList, Button, TouchableHighlight, StatusBar, TextInput } from 'react-native';
+import { AsyncStorage, StyleSheet, Text, View, FlatList, Button, TouchableHighlight, StatusBar, TextInput } from 'react-native';
 import { createBottomTabNavigator, createStackNavigator, createAppContainer, createDrawerNavigator } from 'react-navigation';
 
 /**
@@ -8,8 +8,9 @@ import { createBottomTabNavigator, createStackNavigator, createAppContainer, cre
 const decklist = [
   {
     key: '1',
+    id: 'deck-1',
     title: 'Deck 1',
-    itemsCount: 3,
+    cardsCount: 3,
     cards: [
       {
         question: 'What is React?',
@@ -27,19 +28,40 @@ const decklist = [
   },
   {
     key: '2',
+    id: 'deck-2',
     title: 'Deck 2',
-    itemsCount: 0,
+    cardsCount: 0,
     cards: [],
   },
 ]
-
+const storeData = async (key, value) => {
+  try {
+    await AsyncStorage.mergeItem(key, value);
+  } catch (error) {
+    // Error retrieving data
+    console.log(error.message);
+  }
+}
+const fetchData = async (key) => {
+  let data = null;
+  try {
+    data = await AsyncStorage.getItem(key) || null;
+  } catch (error) {
+    console.log(error.message);
+  }
+  return data;
+}
 
 /**
  * Components
  */
 class DeckItem extends Component {
   onPress = (ev) => {
-    this.props.navigation.navigate('Deck');
+    this.props.navigation.navigate('Deck', {
+      deckId: this.props.id,
+      deckTitle: this.props.title,
+      cardsCount: this.props.cardsCount,
+    });
   }
 
   render() {
@@ -47,7 +69,7 @@ class DeckItem extends Component {
       <TouchableHighlight onPress={this.onPress}>
         <View>
           <Text>{this.props.title}</Text>
-          <Text>{this.props.itemsCount}</Text>
+          <Text>{this.props.cardsCount}</Text>
         </View>
       </TouchableHighlight>
     )
@@ -55,16 +77,45 @@ class DeckItem extends Component {
 }
 
 class DeckList extends Component {
+  state = {
+    decklist: {}
+  }
+
   renderDeckItem = (item) => {
     return (
-      <DeckItem title={item.title} itemsCount={item.itemsCount} navigation={this.props.navigation} />
+      <DeckItem id={item.id} title={item.title} cardsCount={item.cards.length} navigation={this.props.navigation} />
     )
+  }
+
+  componentDidMount () {
+    this.setState({ decklist: this.props.decklist });
+
+    // AsyncStorage not working
+    // const test = fetchData('DECK_LIST');
+    // // debugger;
+    // AsyncStorage.setItem('DECK_LIST', JSON.stringify(decklist), error => {
+    //   debugger;
+    // });
+    // AsyncStorage.getItem('DECK_LIST', () => {
+    //   debugger;
+    // }).then(data => {
+    //   console.log(data);
+    //   debugger;
+    // });
+    // AsyncStorage.getAllKeys('DECK_LIST').then(data => {
+    //   console.log(data);
+    //   debugger;
+    // });
+
+    // this.setState({ decklist: AsyncStorage.getItem('DECK_LIST') });
+    // AsyncStorage.setItem('DECK_LIST', decklist);
+
   }
 
   render() {
     return (
       <FlatList
-        data={decklist}
+        data={this.state.decklist}
         renderItem={({item}) => this.renderDeckItem(item)}
       />
     )
@@ -72,20 +123,41 @@ class DeckList extends Component {
 }
 
 class Deck extends Component {
+  state = {
+    id: '',
+    title: '',
+    cardsCount: 0,
+  }
+
   startQuiz = (ev) => {
-    this.props.navigation.navigate('Quiz');
+    this.props.navigation.navigate('Quiz', {
+      deckId: this.state.id,
+    });
   }
 
   addCard = (ev) => {
-    this.props.navigation.navigate('AddCard');
+    this.props.navigation.navigate('AddCard', {
+      deckTitle: this.state.title,
+    });
+  }
+
+  componentDidMount () {
+    const id = this.props.navigation.getParam('deckId');
+    const title = this.props.navigation.getParam('deckTitle');
+    const cardsCount = this.props.navigation.getParam('cardsCount');
+    this.setState({
+      id,
+      title,
+      cardsCount,
+    });
   }
 
   render() {
     return (
       <View>
         <View>
-          <Text>Deck Title</Text>
-          <Text>3 cards</Text>
+          <Text>{this.state.title}</Text>
+          <Text>{this.state.cardsCount} cards</Text>
         </View>
         <View>
           <Button title={'Start Quiz!'} onPress={this.startQuiz} />
@@ -186,6 +258,7 @@ class AddCard extends Component {
  * Views
  */
 class HomeView extends Component {
+
   static navigationOptions = {
     title: 'Home',
   };
@@ -197,7 +270,7 @@ class HomeView extends Component {
           barStyle="light-content"
           backgroundColor="orange"
         />
-        <DeckList navigation={this.props.navigation} />
+        <DeckList decklist={decklist} navigation={this.props.navigation} />
         <Button title="Add Deck" onPress={ev => this.props.navigation.navigate('AddDeck')} />
       </View>
     );
@@ -205,8 +278,11 @@ class HomeView extends Component {
 }
 
 class DeckView extends Component {
-  static navigationOptions = {
-    title: 'DECK_TITLE',
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('deckTitle'),
+    };
   };
 
   render() {
@@ -247,8 +323,12 @@ class AddDeckView extends Component {
 }
 
 class AddCardView extends Component {
-  static navigationOptions = {
-    title: 'Add Card',
+
+  static navigationOptions = ({ navigation }) => {
+    deckTitle = navigation.getParam('deckTitle');
+    return {
+      title: `Add a new card to ${deckTitle}`,
+    };
   };
 
   render() {
